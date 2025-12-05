599035840ac66cc2105838eac6bb46a1d92f039d
sharmayogesh152004@gmail.com <sharmayogesh152004@gmail.com>
2025-12-05 22:15:53 +0530

    new task

---
+++ DOM Assigments/Task-9/script.js
@@ -0,0 +1,14 @@
+let main = document.querySelector('main');
+
+main.addEventListener('mousemove' ,() => {
+//     let div = document.createElement('div');
+
+
+// // console.log(div);
+
+//     main.appendChild(div);
+
+
+
+    
+})
